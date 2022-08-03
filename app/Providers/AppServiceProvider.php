e746f3448d6fcc219f88aaee2ee0b4d32d443c90
Danyel Alkeddah <14123621+danyelkeddah@users.noreply.github.com>
2022-08-03 13:02:14 +0300

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment('local')) {
+        if ($this->app->environment('local', 'development')) {
             $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
             $this->app->register(TelescopeServiceProvider::class);
         }
