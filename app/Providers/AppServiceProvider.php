c1c9c7e26b23deafa389abbfab0f08055f3b732b
Danyel Alkeddah <14123621+danyelkeddah@users.noreply.github.com>
2022-08-03 13:57:49 +0300

    fixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (!$this->app->environment('local')) {
+        if ($this->app->environment('production', 'development')) {
             URL::forceScheme('https');
         }
     }
