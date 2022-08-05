68ad10306a28f0429b188a8daa2f40c2f86b1e99
Danyel Alkeddah <14123621+danyelkeddah@users.noreply.github.com>
2022-08-05 11:33:31 +0300

    add more users

--- database/seeders/DemoUsersSeeder.php
+++ database/seeders/DemoUsersSeeder.php
@@ -42,6 +42,18 @@ class DemoUsersSeeder extends Seeder
             ],
             [
                 'id' => Str::uuid(),
+                'name' => 'Tarik',
+                'last_name' => 'Al Hafez',
+                'email' => 'tarik@alternativa.dev',
+                'image' => null,
+                'username' => 'tarik',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => Str::uuid(),
                 'name' => 'Yaman',
                 'last_name' => 'Jamal',
                 'email' => 'yaman.jamal@alternativa.dev',
