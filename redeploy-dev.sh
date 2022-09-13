fd6e0eab35016367e35fc0a9d91b1051e388b2ca
Danyel Alkeddah <14123621+danyelkeddah@users.noreply.github.com>
2022-09-13 12:46:35 +0300

    update redeploy script

--- redeploy-dev.sh
+++ redeploy-dev.sh
@@ -3,4 +3,8 @@ cd /data/apps/game-hub || exit
 sudo docker compose down
 git stash
 git pull
+sudo docker compose run --rm composer install --ignore-platform-req=ext-sockets
+sudo docker compose run --rm npm install
+npm run prod
 sudo docker compose up -d
+sudo docker compose run --rm artisan wodo:gamehub-demo-seed
