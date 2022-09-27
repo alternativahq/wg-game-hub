65731899cd906a87cced33201281d69b9e7118ae
Danyel Alkeddah <14123621+danyelkeddah@users.noreply.github.com>
2022-09-27 10:43:25 +0300

    only userid and username for game lobby users list

--- app/Http/Resources/GameLobbyUserResource.php
+++ app/Http/Resources/GameLobbyUserResource.php
@@ -11,13 +11,8 @@ class GameLobbyUserResource extends JsonResource
     public function toArray($request): array
     {
         return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'image' => $this->image,
-            'imageUrl' => $this->image_url,
+            'userId' => $this->id,
             'username' => $this->username,
-            'lastName' => $this->last_name,
-            'fullName' => $this->full_name,
         ];
     }
 }
