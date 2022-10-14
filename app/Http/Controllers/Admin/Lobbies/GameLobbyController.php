5762754a96eec8262b78d893fd1cccd0bd653fb2
Danyel Alkeddah <14123621+danyelkeddah@users.noreply.github.com>
2022-10-14 13:41:21 +0300

    convert game play duration to seconds

--- app/Http/Controllers/Admin/Lobbies/GameLobbyController.php
+++ app/Http/Controllers/Admin/Lobbies/GameLobbyController.php
@@ -63,7 +63,7 @@ class GameLobbyController extends Controller
                 'scheduled_at' => $request->scheduledAt,
                 'algorithm_id' => $request->algorithmId,
                 'start_at' => $request->startsAt,
-                'game_play_duration' => $request->gamePlayDuration / 60,
+                'game_play_duration' => $request->gamePlayDuration,
             ])
             ->except('asset')
             ->toArray();
