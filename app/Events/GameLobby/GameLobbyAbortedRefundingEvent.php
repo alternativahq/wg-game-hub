c28fee76c583a3b48e99aaf5ca148a063011c890
yamanjamal <yamanjamal0@hotmail.com>
2022-11-02 15:17:09 +0200

    done with the ui and with the events, enums , controllers and with the task

---
+++ app/Events/GameLobby/GameLobbyAbortedRefundingEvent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events\GameLobby;
+
+use App\Models\GameLobby;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use App\Http\Resources\GameLobbyResource;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class GameLobbyAbortedRefundingEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(public GameLobby $gameLobby, public $cause)
+    {
+    }
+
+    public function broadcastOn(): Channel
+    {
+        return new PresenceChannel('game-lobby.' . $this->gameLobby->id);
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'status.game-aborted-refunding';
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'cause' => $this->cause,
+        ];
+    }
+}
