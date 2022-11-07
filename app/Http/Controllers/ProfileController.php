6fde18bc6d8f49fed1da3c1e846413ea1be01e70
yamanjamal <yamanjamal0@hotmail.com>
2022-11-07 13:51:30 +0200

    adding the api end point

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Inertia\Inertia;
+use App\Services\Internal\UserProfileAPI;
 
 class ProfileController extends Controller
 {
+    public function __construct(protected UserProfileAPI $userProfileAPI){
+    }
+
     public function __invoke()
     {
-        return Inertia::render('Profile');
+        $response = $this->userProfileAPI->userProfileInfo();
+        // dd($response->json());
+        return Inertia::render('Profile', [
+            'userProfileInfo' => $response->json(),
+        ]);
     }
 }
