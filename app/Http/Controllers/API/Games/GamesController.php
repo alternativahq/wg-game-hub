9b34711ba3241fc06e57bab2ce1f648d33ffe062
yamanjamal <117714704+yamanjamal2@users.noreply.github.com>
2022-11-11 15:13:36 +0200

    adding games end point resource

--- app/Http/Controllers/API/Games/GamesController.php
+++ app/Http/Controllers/API/Games/GamesController.php
@@ -2,24 +2,59 @@
 
 namespace App\Http\Controllers\API\Games;
 
+use App\Models\Game;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\GameResource;
-use App\Models\Game;
+use App\Http\Requests\StoreGameRequest;
+use App\Http\Resources\GameAPIResource;
+use App\Http\Requests\UpdateGameRequest;
 
 class GamesController extends Controller
 {
     public function index()
     {
-        $games = Game::online()
-            ->withCount('gameLobbies')
-            ->select(['id', 'name', 'image'])
-            ->paginate();
+        $games =  Game::withTrashed()->get();
 
-        return GameResource::collection($games);
+        // $games = GamesPipeline::make(
+        //     builder: Game::query(),
+        //     request: $request,
+        // )->withTrashed()->paginate();
+
+                // 'q',
+                // 'date',
+                // 'sort_by',
+                // 'sort_order',
+                // 'min_players',
+                // 'max_players',
+                // 'min_base_entrance_fee',
+                // 'max_base_entrance_fee',
+                // 'games_gamelobbies_type',
+                // 'games_gamelobbies_asset_symbol',
+
+        return GameAPIResource::collection($games);
     }
 
     public function show(Game $game)
     {
-        return new GameResource($game);
+        return response()->json(new GameAPIResource(resource: $game));
+    }
+
+
+    public function store(StoreGameRequest $request)
+    {
+        $game = Game::create($request->validated());
+        return response()->json(new GameAPIResource($game));
+    }
+
+    public function update(UpdateGameRequest $request, Game $game)
+    {
+        $game->update($request->validated());
+        return response()->json(new GameAPIResource(resource: $game));
+    }
+
+    public function destroy(Game $game)
+    {
+        $game->delete();
+        return response()->noContent();
     }
 }
