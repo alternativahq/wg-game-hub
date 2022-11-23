d8872fea393966f7ce471d4d5ffb36bae4757db9
yamanjamal <117714704+yamanjamal2@users.noreply.github.com>
2022-11-23 12:14:25 +0200

    changed the session id to nullable

---
+++ database/migrations/2022_11_23_092251_change_session_id_to_null_in_game_lobbies_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('game_lobbies', function (Blueprint $table) {
+            $table
+                ->longText('session_id')
+                ->nullable()
+                ->change();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('game_lobbies', function (Blueprint $table) {});
+    }
+};
