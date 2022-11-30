5ad42288ebd0690386e1db8c888be84345338295
yamanjamal <117714704+yamanjamal2@users.noreply.github.com>
2022-11-30 16:13:36 +0200

    upgraded hero icon package and fixed the ui

--- resources/js/Pages/Games/Lobbies/Details.vue
+++ resources/js/Pages/Games/Lobbies/Details.vue
@@ -4,6 +4,10 @@ import { Inertia } from '@inertiajs/inertia';
 import { Link } from '@inertiajs/inertia-vue3';
 import { useCurrentUser } from '@/Composables/useCurrentUser';
 import BorderedContainer from '@/Shared/BorderedContainer';
+import { TrophyIcon } from '@heroicons/vue/24/solid';
+import { CurrencyDollarIcon } from '@heroicons/vue/24/solid';
+import { Bars4Icon } from '@heroicons/vue/24/solid';
+import { CalendarDaysIcon } from '@heroicons/vue/24/solid';
 
 let currentUser = useCurrentUser();
 let dayjs = inject('dayjs');
@@ -40,8 +44,8 @@ orderedPlayers = _.concat(
 <template>
     <div>
         <BorderedContainer class="bg-green-200 mb-10">
-            <div class="divide-y-2 rounded-lg bg-gray-50 p-6">
-                <div class="flex flex-col  md:flex-row item-start gap-16 mt-10 mb-5">
+            <div class="flex flex-col md:flex-row rounded-lg bg-gray-50 p-6">
+                <div class="w-4/5 mr-5 flex flex-col md:flex-row item-start gap-16 mt-10 mb-5">
                     <div class="mb-5">
                         <img :src="gameLobby.data.game.image_url" :alt="`${gameLobby.data.game.name} art`"
                             class="aspect-[16/9] rounded-lg md:mb-0 max-h-[40.25rem] md:w-full md:max-h-[10.25rem]"
@@ -56,8 +60,82 @@ orderedPlayers = _.concat(
                         </div>
                     </div>
                 </div>
+                <div class="w-1/5 space-y-6 divide-y-2 font-semibold">
+                    <div class="flex gap-2">
+                        <div><Bars4Icon class="h-5 w-5"/></div>
+                        <div>{{gameLobby.data.name?.substring(0,25)+ '...' }}</div>
+                    </div>
+                    <div class="flex gap-2">
+                        <div><TrophyIcon class="h-5 w-5"/></div>
+                        <div>{{ currentUserScore.rank }} place</div>
+                    </div>
+                    <div class="flex gap-2">
+                        <div><CurrencyDollarIcon class="h-5 w-5"/></div>
+                        <div>{{ currentUserScore.rank }} earned</div>
+                    </div>
+                    <div class="flex gap-2">
+                        <div><CalendarDaysIcon class="h-5 w-5"/></div>
+                        <div>Played At {{
+                                dayjs(gameLobby.data.start_at)
+                                .utc()
+                                .local()
+                                .tz(dayjs.tz.guess())
+                                .format('H:s YYYY/MM/DD')
+                            }}
+                        </div>
+                    </div>
+                </div>
             </div>
         </BorderedContainer>
+        <div>
+            <div class="text-3xl font-bold mb-6">Lobby Details</div>
+            <BorderedContainer class="bg-blue-200 mb-10">
+                <div class="flex gap-2 rounded-lg bg-gray-50 p-6">
+                    <div class="w-1/2">
+                        <div class=" space-y-6 divide-y-2 font-semibold">
+                            <div class="flex gap-2">
+                                <div><Bars4Icon class="h-5 w-5"/></div>
+                                <div>{{gameLobby.data.name?.substring(0,25)+ '...' }}</div>
+                            </div>
+                            <div class="flex gap-2">
+                                <div><TrophyIcon class="h-5 w-5"/></div>
+                                <div>{{ currentUserScore.rank }} place</div>
+                            </div>
+                            <div class="flex gap-2">
+                                <div><CurrencyDollarIcon class="h-5 w-5"/></div>
+                                <div>{{ currentUserScore.rank }} earned</div>
+                            </div>
+                            <div class="flex gap-2">
+                                <div><CalendarDaysIcon class="h-5 w-5"/></div>
+                                <div>Played At {{
+                                        dayjs(gameLobby.data.start_at)
+                                        .utc()
+                                        .local()
+                                        .tz(dayjs.tz.guess())
+                                        .format('H:s YYYY/MM/DD')
+                                    }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-1/2">
+                        <BorderedContainer class="bg-blue-200 mb-10">
+                            <div class="flex justify-around gap-2 rounded-lg bg-gray-50 p-6">
+                                <div v-for="player in orderedPlayers" :key="player.id" class="flex flex-row md:flex-col">
+                                    <div class="w-1/3">
+                                        <img :src="player.image_url" :alt="`${player.image_url} art`"
+                                            class="h-12 w-12 object-cover md:h-24 md:w-24"
+                                        />
+                                    </div>
+                                    <div class="w-1/3">{{player.name}}</div>
+                                    <div class="w-1/3">{{player.ranmk}}</div>
+                                </div>
+                            </div>
+                        </BorderedContainer>
+                    </div>
+                </div>
+            </BorderedContainer>
+        </div>
         <BorderedContainer class="bg-green-200 mb-10">
             <div class="divide-y-2 rounded-lg bg-gray-50 p-2">
                 <div class="text-center text-3xl font-bold mb-10">Top Players</div>
